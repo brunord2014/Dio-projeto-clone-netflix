57ecaef6c3f209eca887eb704ed90ea59f6b204e
Bruno Rodrigues <brunord2014@hotmail.com>
2022-02-08 13:22:30 -0300

    Desafio de recriar a interface da Netflix V1.0

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 60,
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
